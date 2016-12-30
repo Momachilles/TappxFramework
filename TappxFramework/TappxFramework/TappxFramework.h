0c42bb94cb3c0512a9eacc98a387d368f7c230b3
David Alarcon <momachilles@gmail.com>
2016-12-30 17:43:44 +0100

    init

---
+++ TappxFramework/TappxFramework/TappxFramework.h
@@ -0,0 +1,19 @@
+//
+//  TappxFramework.h
+//  TappxFramework
+//
+//  Created by David Alarcon on 30/12/2016.
+//  Copyright © 2016 4Crew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TappxFramework.
+FOUNDATION_EXPORT double TappxFrameworkVersionNumber;
+
+//! Project version string for TappxFramework.
+FOUNDATION_EXPORT const unsigned char TappxFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TappxFramework/PublicHeader.h>
+
+
