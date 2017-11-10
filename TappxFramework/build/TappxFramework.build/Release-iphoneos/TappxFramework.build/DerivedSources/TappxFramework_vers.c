9b1113979e661380581f19a1ae562722380761b4
David Alarcon <momachilles@gmail.com>
2017-11-10 13:27:17 +0100

    Add ignore

---
+++ TappxFramework/build/TappxFramework.build/Release-iphoneos/TappxFramework.build/DerivedSources/TappxFramework_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char TappxFrameworkVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:TappxFramework  PROJECT:TappxFramework-1" "\n"; const double TappxFrameworkVersionNumber __attribute__ ((used)) = (double)1.;
